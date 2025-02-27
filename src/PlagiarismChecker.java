636497dbe4940e3e35800560dd5d29d23e195bff
kate <kate.little@menloschool.org>
2025-02-27 09:20:27 -0800

    2/27 - all tests passed

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -15,29 +15,26 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
-        int[][] longest = new int[doc1.length()][doc2.length()];
+        int[][] longest = new int[doc1.length() + 1][doc2.length() + 1];
         int up;
         int left;
         // Tabulation approach
-        for (int i = 0; i < doc1.length(); i++){
-            for (int j = 0; j < doc2.length(); j++) {
+        for (int i = 1; i <= doc1.length(); i++){
+            for (int j = 1; j <= doc2.length(); j++) {
                 up = 0;
                 left = 0;
-                // Find max of existing saved values in table — look up & left
-                if (i - 1 >= 0) {
+                // If the two most recent chars are equal, increment diagonal value by 1
+                if (doc1.charAt(i - 1) == (doc2.charAt(j - 1))){
+                    longest[i][j] = longest[i - 1][j - 1] + 1;
+                }
+                else{
+                    // Find max of existing saved values in table — look up & left
                     up = longest[i - 1][j];
-                }
-                if (j - 1 >= 0) {
                     left = longest[i][j - 1];
-                }
-                longest[i][j] = Math.max(left, up);
-                // If the two most recent chars are equal, increment value by 1
-                // Don't increment if it's impossible to have a substring that long
-                if (doc1.charAt(i) == (doc2.charAt(j)) && (longest[i][j] <= i && longest[i][j] <= j)){
-                    longest[i][j] += 1;
+                    longest[i][j] = Math.max(left, up);
                 }
             }
         }
-        return longest[doc1.length() - 1][doc2.length() - 1];
+        return longest[doc1.length()][doc2.length()];
     }
 }
